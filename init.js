4579b171701c487c77d5f964852ffc1c23e9b85b
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-11-08 21:36:21 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVz88p0l1BISt2Y7cUL1uAaEDKc-xZedA",
+  authDomain: "tandas-cff24.firebaseapp.com",
+  projectId: "tandas-cff24",
+  storageBucket: "tandas-cff24.firebasestorage.app",
+  messagingSenderId: "677683353366",
+  appId: "1:677683353366:web:7a28f1d32844997d64e237"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
