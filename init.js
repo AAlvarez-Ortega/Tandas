b772bc60aff769fcf65814177bd494f41e05b1dc
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-11-08 21:48:36 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBVz88p0l1BISt2Y7cUL1uAaEDKc-xZedA",
   authDomain: "tandas-cff24.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "677683353366",
   appId: "1:677683353366:web:7a28f1d32844997d64e237"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
