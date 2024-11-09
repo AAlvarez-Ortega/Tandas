1d09854ce67ddd6bcb110e172eda5844dccef5d4
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-11-08 21:55:51 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,3 +1,5 @@
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBVz88p0l1BISt2Y7cUL1uAaEDKc-xZedA",
   authDomain: "tandas-cff24.firebaseapp.com",
@@ -6,3 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "677683353366",
   appId: "1:677683353366:web:7a28f1d32844997d64e237"
 };
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
